3397de6de36a797a2921da3acc22124d0bca0fb8
hooiv <125276621+hooiv@users.noreply.github.com>
2025-01-22 11:23:11 +0000

    Jan 22, 2025, 4:53 PM

---
+++ src/pages/submit-complaint.js
@@ -0,0 +1,10 @@
+// pages/login.js
+import ComplaintForm from '../components/ComplaintForm';
+import Link from 'next/link';
+
+const ComplaintPage = () => (
+  <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      </div>
+);
+
+export default ComplaintPage;
