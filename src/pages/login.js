e4bf849a40c075fba84f6f07e18061337fef9bb8
hooiv <125276621+hooiv@users.noreply.github.com>
2025-01-22 11:41:51 +0000

    Jan 22, 2025, 5:11 PM

--- src/pages/login.js
+++ src/pages/login.js
@@ -5,7 +5,7 @@ import Link from 'next/link';
 const LoginPage = () => (
   <div className="flex flex-col items-center justify-center min-h-screen ">
      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-96">
-         <h1 className="text-2xl font-bold  text-center">Login</h1>
+         <h1 className="text-2xl font-bold mb-6 text-center">Login</h1>
          <LoginForm />
          <div className="mt-4 text-center">
            <p>Don't have an account? <Link href="/register" className="text-blue-500 hover:text-blue-700">Register</Link></p>
